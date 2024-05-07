dc6472bd681d823eff13ad4f87fe5e1a96df3e42
Gabriel <gahalberto@icloud.com>
2024-05-07 15:31:15 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nodets-canil
+Projeto feito nomóudlo do curso Node + typescript
+
+### Pré requisitos globais: 
+
+`npm i -g nodemon typescript`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from 'express';
+
+export const home = (req: Request, res:Response) => {
+    res.send('home');
+}
+
+export const dogs = (req: Request, res:Response) => {
+    //res.render('page/page');
+}
+
+export const cats = (req: Request, res:Response) => {
+    //res.render('page/page');
+}
+
+export const fishes = (req: Request, res:Response) => {
+    //res.render('page/page');
+}
